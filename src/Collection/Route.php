215abb3eabd043bb5d5d59c7d51f6f6e1d3876d4
ignaszak <tomek.ignaszak@gmail.com>
2016-04-10 23:05:02 +0200

    Add getChecksum, addTokens and addPatterns methods, remove tokens method

---
+++ src/Collection/Route.php
@@ -0,0 +1,228 @@
+<?php
+/**
+ *
+ * PHP Version 7.0
+ *
+ * @copyright 2016 Tomasz Ignaszak
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ *
+ */
+declare(strict_types=1);
+
+namespace Ignaszak\Router\Collection;
+
+use Ignaszak\Router\RouterException;
+
+class Route implements IRouteStart, IRouteAdd, IRoute
+{
+
+    /**
+     * Stores added routes
+     *
+     * @var array
+     */
+    private $routeArray = [];
+
+    /**
+     * Global tokens
+     *
+     * @var string[]
+     */
+    private $tokenArray = [];
+
+    /**
+     * Custom regex pattern
+     *
+     * @var string[]
+     */
+    private $patternArray = [];
+
+    /**
+     *
+     * @var string
+     */
+    private $lastName = '';
+
+    /**
+     *
+     * @var string
+     */
+    private $group = '';
+
+    private function __construct()
+    {
+    }
+
+    /**
+     *
+     * @return IRouteStart
+     */
+    public static function start(): IRouteStart
+    {
+        return new Route();
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     * @see \Ignaszak\Router\Interfaces\IRoute::getRouteArray()
+     */
+    public function getRouteArray(): array
+    {
+        return [
+            'routes' => $this->routeArray,
+            'tokens' => $this->tokenArray,
+            'patterns' => $this->patternArray,
+            'checksum' => $this->getChecksum()
+        ];
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     * @see \Ignaszak\Router\Interfaces\IRouteStart::add($name, $pattern)
+     */
+    public function add(
+        string $name = null,
+        string $pattern,
+        string $method = ''
+    ): IRouteAdd {
+        if (is_null($name)) {
+            $this->routeArray[] = ['path' => $pattern];
+            // Last array key
+            $name = key(array_slice($this->routeArray, -1, 1, true));
+        } else {
+            if (array_key_exists($name, $this->routeArray)) {
+                throw new RouterException(
+                    "Route name '{$name}' alredy exists"
+                );
+            }
+            $this->routeArray[$name] = ['path' => $pattern];
+        }
+        $this->routeArray[$name]['group'] = $this->group;
+        $this->routeArray[$name]['method'] = $method;
+        $this->lastName = $name;
+
+        return $this;
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     * @see \Ignaszak\Router\Interfaces\IRouteStart::get($name, $pattern)
+     */
+    public function get(string $name = null, string $pattern): IRouteAdd
+    {
+        $this->add($name, $pattern, 'GET');
+
+        return $this;
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     * @see \Ignaszak\Router\Interfaces\IRouteStart::post($name, $pattern)
+     */
+    public function post(string $name = null, string $pattern): IRouteAdd
+    {
+        $this->add($name, $pattern, 'POST');
+
+        return $this;
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     * @see \Ignaszak\Router\Interfaces\IRouteAdd::controller($controller)
+     */
+    public function controller(string $controller): IRouteAdd
+    {
+        $this->routeArray[$this->lastName]['controller'] = $controller;
+
+        return $this;
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     * @see \Ignaszak\Router\Interfaces\IRouteAdd::tokens($tokens)
+     */
+    public function tokens(array $tokens): IRouteAdd
+    {
+        $this->routeArray[$this->lastName]['tokens'] = array_merge(
+            $this->routeArray[$this->lastName]['tokens'] ?? [],
+            $tokens
+        );
+
+        return $this;
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     * @see \Ignaszak\Router\Interfaces\IRouteAdd::attach($closure)
+     */
+    public function attach(\Closure $closure, bool $call = true): IRouteAdd
+    {
+        $this->routeArray[$this->lastName]['callAttachment'] = $call;
+        $this->routeArray[$this->lastName]['attachment'] = $closure;
+
+        return $this;
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     * @see \Ignaszak\Router\Interfaces\IRouteStart::group($name)
+     */
+    public function group(string $name = ''): IRouteStart
+    {
+        $this->group = $name;
+
+        return $this;
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     * @see \Ignaszak\Router\Interfaces\IRouteStart::addTokens($tokens)
+     */
+    public function addTokens(array $tokens): IRouteStart
+    {
+        $this->tokenArray = array_merge(
+            $this->tokenArray,
+            $tokens
+        );
+
+        return $this;
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     * @see \Ignaszak\Router\Interfaces\IRouteStart::addPatterns($patterns)
+     */
+    public function addPatterns(array $patterns): IRouteStart
+    {
+        $this->patternArray = array_merge(
+            $this->patternArray,
+            $patterns
+        );
+
+        return $this;
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     * @see \Ignaszak\Router\Interfaces\IRoute::getChecksum()
+     */
+    public function getChecksum(): string
+    {
+        return md5(json_encode([
+            'routes' => $this->routeArray,
+            'tokens' => $this->tokenArray,
+            'patterns' => $this->patternArray
+        ]));
+    }
+}
---
+++ tests/Collection/RouteTest.php
@@ -0,0 +1,318 @@
+<?php
+namespace Test\Collection;
+
+use Ignaszak\Router\Collection\Route;
+
+class RouteTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     *
+     * @var Route
+     */
+    private $route;
+
+    public function setUp()
+    {
+        $this->route = Route::start();
+    }
+
+    public function testAddWithName()
+    {
+        $this->route->add('name1', '/pattern/subpattern', 'AnyHttpMethod');
+        $this->assertEquals(
+            'name1',
+            \PHPUnit_Framework_Assert::readAttribute($this->route, 'lastName')
+        );
+        $this->route->add('name2', '/pattern');
+        $this->assertEquals(
+            'name2',
+            \PHPUnit_Framework_Assert::readAttribute($this->route, 'lastName')
+        );
+
+        $this->assertEquals(
+            [
+                'name1' => [
+                    'path' => '/pattern/subpattern',
+                    'group' => '',
+                    'method' => 'AnyHttpMethod'
+                ],
+                'name2' => [
+                    'path' => '/pattern',
+                    'group' => '',
+                    'method' => ''
+                ]
+            ],
+            \PHPUnit_Framework_Assert::readAttribute(
+                $this->route,
+                'routeArray'
+            )
+        );
+    }
+
+    public function testAddWithoutNAme()
+    {
+        $this->route->add(null, '/pattern/subpattern');
+        $this->assertEquals(
+            0,
+            \PHPUnit_Framework_Assert::readAttribute($this->route, 'lastName')
+        );
+        $this->route->add(null, '/pattern');
+        $this->assertEquals(
+            1,
+            \PHPUnit_Framework_Assert::readAttribute($this->route, 'lastName')
+        );
+
+        $this->assertEquals(
+            [
+                0 => [
+                    'path' => '/pattern/subpattern',
+                    'group' => '',
+                    'method' => ''
+                ],
+                1 => [
+                    'path' => '/pattern',
+                    'group' => '',
+                    'method' => ''
+                ]
+            ],
+            \PHPUnit_Framework_Assert::readAttribute(
+                $this->route,
+                'routeArray'
+            )
+        );
+    }
+
+    public function testGet()
+    {
+        $this->route->get(null, '/pattern/subpattern');
+        $this->assertEquals(
+            'GET',
+            $this->route->getRouteArray()['routes'][0]['method']
+        );
+    }
+
+    public function testPost()
+    {
+        $this->route->post(null, '/pattern/subpattern');
+        $this->assertEquals(
+            'POST',
+            $this->route->getRouteArray()['routes'][0]['method']
+        );
+    }
+
+    /**
+     * @expectedException \Ignaszak\Router\RouterException
+     */
+    public function testAddDuplicateName()
+    {
+        $this->route->add('name', '/anyPattern');
+        $this->route->add('name', '/anyPattern');
+    }
+
+    public function testAddController()
+    {
+        $this->route->add('anyName', '/anyPattern')->controller('anyController');
+        $this->assertEquals(
+            [
+                'anyName' => [
+                    'path' => '/anyPattern',
+                    'controller' => 'anyController',
+                    'group' => '',
+                    'method' => ''
+                ],
+            ],
+            \PHPUnit_Framework_Assert::readAttribute(
+                $this->route,
+                'routeArray'
+            )
+        );
+    }
+
+    public function testAddTokensToRoute()
+    {
+        $this->route->add('anyName', '/anyPattern')->tokens([
+            'tokenName1' => 'pattern1',
+            'tokenName2' => 'pattern2',
+            'tokenName3' => 'pattern3'
+        ]);
+        $this->assertEquals(
+            [
+                'anyName' => [
+                    'path' => '/anyPattern',
+                    'tokens' => [
+                        'tokenName1' => 'pattern1',
+                        'tokenName2' => 'pattern2',
+                        'tokenName3' => 'pattern3'
+                    ],
+                    'group' => '',
+                    'method' => ''
+                ],
+            ],
+            \PHPUnit_Framework_Assert::readAttribute(
+                $this->route,
+                'routeArray'
+            )
+        );
+    }
+
+    public function testAttach()
+    {
+        $anyAttachment = function () {
+        };
+        $this->route->add('anyName', '/anyPattern')->attach($anyAttachment);
+        $this->assertEquals(
+            [
+                'anyName' => [
+                    'path' => '/anyPattern',
+                    'callAttachment' => true,
+                    'attachment' => $anyAttachment,
+                    'group' => '',
+                    'method' => ''
+                ],
+            ],
+            \PHPUnit_Framework_Assert::readAttribute(
+                $this->route,
+                'routeArray'
+            )
+        );
+    }
+
+    public function testCallableAttach()
+    {
+        $anyAttachment = function () {
+        };
+        $this->route->add('anyName', '/anyPattern')
+            ->attach($anyAttachment, false);
+        $this->assertEquals(
+            [
+                'anyName' => [
+                    'path' => '/anyPattern',
+                    'callAttachment' => false,
+                    'attachment' => $anyAttachment,
+                    'group' => '',
+                    'method' => ''
+                ],
+            ],
+            \PHPUnit_Framework_Assert::readAttribute(
+                $this->route,
+                'routeArray'
+            )
+        );
+    }
+
+    public function testGroup()
+    {
+        $this->route->group('anyGroupName');
+        $this->route->add(null, '/anyPattern');
+        $this->route->add(null, '/anyPattern');
+        $this->assertEquals(
+            [
+                [
+                    'path' => '/anyPattern',
+                    'group' => 'anyGroupName',
+                    'method' => ''
+                ],
+                [
+                    'path' => '/anyPattern',
+                    'group' => 'anyGroupName',
+                    'method' => ''
+                ]
+            ],
+            \PHPUnit_Framework_Assert::readAttribute(
+                $this->route,
+                'routeArray'
+            )
+        );
+    }
+
+    public function testClearGroup()
+    {
+        $this->route->group('anyGroupName');
+        $this->route->add(null, '/anyPattern');
+        $this->route->group();
+        $this->route->add(null, '/anyPattern');
+        $this->assertEquals(
+            [
+                [
+                    'path' => '/anyPattern',
+                    'group' => 'anyGroupName',
+                    'method' => ''
+                ],
+                [
+                    'path' => '/anyPattern',
+                    'group' => '',
+                    'method' => ''
+                ]
+            ],
+            \PHPUnit_Framework_Assert::readAttribute(
+                $this->route,
+                'routeArray'
+            )
+        );
+    }
+
+    public function testAddTokens()
+    {
+        $tokens = [
+            'tokenName1' => 'pattern1',
+            'tokenName2' => 'pattern2',
+            'tokenName3' => 'pattern3'
+        ];
+        $this->route->addTokens($tokens);
+        $this->assertEquals(
+            $tokens,
+            \PHPUnit_Framework_Assert::readAttribute(
+                $this->route,
+                'tokenArray'
+            )
+        );
+    }
+
+    public function testAddPatterns()
+    {
+        $patterns = [
+            'name1' => 'testPattern1',
+            'name2' => 'testPattern2',
+            'name3' => 'testPattern3',
+        ];
+        $this->route->addPatterns($patterns);
+        $patternArray = \PHPUnit_Framework_Assert::readAttribute(
+            $this->route,
+            'patternArray'
+        );
+        $this->assertTrue(
+            in_array('testPattern1', $patternArray) &&
+            in_array('testPattern2', $patternArray) &&
+            in_array('testPattern3', $patternArray)
+        );
+    }
+
+    public function testGetChecksum()
+    {
+        $this->route->add(null, 'anypattern')->tokens(['token' => 'pattern']);
+        $this->route->addTokens(['token' => 'pattern']);
+        $this->route->addPatterns(['pattern' => 'regex']);
+        $this->assertEquals(
+            md5(json_encode([
+            'routes' => [
+            0 => [
+            'path' => 'anypattern',
+            'group' => '',
+            'method' => '',
+            'tokens' => [
+            'token' => 'pattern'
+            ]
+                    ]
+            ],
+            'tokens' => [
+            'token' => 'pattern'
+            ],
+            'patterns' => [
+            'pattern' => 'regex'
+            ]
+            ])),
+            $this->route->getChecksum()
+        );
+    }
+}
